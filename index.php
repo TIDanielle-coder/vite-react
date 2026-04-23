c74e4af38d8581cea840f1e2d37a1d136746dfcd
TIDanielle-coder <daniel240ilesanmi@gmail.com>
2026-04-23 13:42:56 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Full-Stack Login & Register Form With User & Admin</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="form-box active" id="login-form">
+            <form action="">
+                <h2>Login</h2>
+                <input type="email" name="email" placeholder="email" required>
+                <input type="password" name="password" placeholder="password" required>
+                <button type="submit" Name="login">Login</button>
+                <p>Don't have an account? <a href="#" onclick="showform('register-form')">Register</a></p>
+            </form>    
+        </div>
+        <div class="form-box" id="register-form">
+            <form action="">
+                <h2>Register</h2>
+                <input type="name" name="name" placeholder="Name" required>
+                <input type="email" name="email" placeholder="email" required>
+                <input type="password" name="password" placeholder="password" required>
+                <select name="role">
+                    <option value="">---Select Role-----</option>
+                    <option value="User">User</option>
+                    <option value="Admin">Admin</option>
+                </select>
+                <button type="submit" Name="login">Register</button>
+                <p> Already have an account? <a href="#" onclick="showform('login-form')">Login</a></p>
+            </form>    
+        </div>
+    </div>
+<script src="script.js"></script>
+</body>
+</html>
