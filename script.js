73cbf22a036ac7fd2412fec3d582829f4fd1b490
TIDanielle-coder <daniel240ilesanmi@gmail.com>
2026-04-23 13:54:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function showform(formid) {
+function showform(formid) { 
     document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
     document.getElementById(formid).classList.add("active");
-}
+}  
---
+++ user_page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class="wrapper">
+        <nav class="nav">
+            <div class="nav-logo">
+                <p>LOGO .</p>
+            </div>
+            <div class="nav-menu">
+              <ul>
+                <li><a href="#" class="link active">Home</a></li>
+                <li><a href="blog.html" class="link">Blog</a></li>
+                <li><a href="contact US.html" class="link">Contact us</a></li>
+                <li><a href="Profile.html" class="link">Profile</a></li>
+              </ul>
+            </div>
+            <div class="nav-button">
+                <button class="btn white-btn" id="LoginBtn"><a href="form.html" class="link">Sign in</a></button>
+                <button class="btn" id="registration"><a href="form.html" class="link"></a>Sign Up</button>
+            </div>
+            <div class="nav-menu-btn">
+               <i class="bx bx-menu"></i>
+            </div>
+        </nav>
+</body>
+</html>
